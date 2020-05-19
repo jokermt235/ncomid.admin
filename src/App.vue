4c3fdb66acde533eb4702f59422746d92538307a
mutalip <jokermt2357@gmail.com>
2020-05-19 17:55:24 +0600

    Apply admin pannel

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
+import styles from '@/assets/css/bootstrap.min.css';
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1>Hello world</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
