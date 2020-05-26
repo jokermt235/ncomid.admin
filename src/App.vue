7aa21e84da060f660394e4699e56c8196e85485b
mutalip <jokermt2357@gmail.com>
2020-05-26 17:54:55 +0600

    Adding main page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
-import styles from '@/assets/css/bootstrap.min.css';
+
 <template>
   <div id="app">
-      <h1>Hello world</h1>
+    <HomePage></HomePage>
   </div>
 </template>
 
 <script>
-
+import HomePage from '@/components/HomePage.vue';
 export default {
-  name: 'App',
   components: {
+    HomePage
   }
 }
 </script>
+d
