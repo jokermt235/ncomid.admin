6f8d440c8ba2377ffc94c43a167a270aaeb29a7e
root <root@244275.simplecloud.ru>
2020-06-02 14:39:00 +0300

    Adding event to component

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,7 +4,30 @@
         <div class="ContentFluid">
             <div class="row flex-xl-nowrap">
                 <div class="col-md-3 col-xl-2 bd-sidebar">
-                    <LeftPanel></LeftPanel>
+                    <div class="LeftPanel">
+                        <form class="bd-search d-flex align-items-center">
+                            <input type="search" class="form-control ds-input" id="search-input" 
+                            placeholder="Search..."/>
+                        </form>
+                        <hr/>
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#" @click="loadBanners">Баннеры</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Новости</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Сообщения</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Фотогалерея</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Отзывы</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content">
                     <MiddleContent></MiddleContent>
@@ -16,19 +39,26 @@
 <script>
 import style from '@/assets/css/bootstrap.min.css';
 import TopMenu from '@/components/TopMenu.vue';
-import LeftPanel from '@/components/LeftPanel.vue';
 import MiddleContent from '@/components/MiddleContent.vue';
 export default {
-  components: {
-    TopMenu,
-    LeftPanel,
-    MiddleContent
-  }
+    components: {
+        TopMenu,
+        MiddleContent
+    },
+    methods:{
+        loadBanners(){
+            alert("HELLO FROM COMPONENT");
+        }
+    }
 }
 </script>
 <style scoped>
     .ContentFluid{
         padding-right: 15px;
         padding-left: 15px;
+    }
+    .LeftPanel{
+        margin-top:16px;
+        margin-left:16px;
     }
 </style>
--- src/components/LeftPanel.vue
+++ src/components/LeftPanel.vue
@@ -1,28 +1,5 @@
 <template>
-    <div class="LeftPanel">
-        <form class="bd-search d-flex align-items-center">
-            <input type="search" class="form-control ds-input" id="search-input" 
-            placeholder="Search..."/>
-        </form>
-        <hr/>
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link active" href="#" @click="loadBanners">Баннеры</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Новости</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Сообщения</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Фотогалерея</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Отзывы</a>
-            </li>
-        </ul>
-    </div>
+    
 </template>
 <script>
     export default{
@@ -33,8 +10,5 @@
     }
 </script>
 <style scoped>
-    .LeftPanel{
-        margin-top:16px;
-        margin-left:16px;
-    }
+    
 </style>
